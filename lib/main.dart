76ab5a4c68712d5972915d4eaf97180ae8ed58d6
Gonzalo Bally <6zo.bally@gmail.com>
2024-02-26 13:04:52 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:template_app/blocs/blocs.dart';
+import 'package:template_app/data/data.dart';
+import 'package:template_app/services/services.dart';
+import 'package:template_app/views/views.dart';
+
+Future<void> main() async {
+  await HiveService.initializeHive();
+  runApp(const Main());
+}
+
+// This widget initializes the BLoC providers and runs the app.
+class Main extends StatelessWidget {
+  const Main({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => UserBloc(
+            userRepository: UserRepository(),
+          ),
+        ),
+      ],
+      child: const App(),
+    );
+  }
+}
---
+++ lib/theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+ThemeData theme = ThemeData(
+  primaryColor: Colors.white,
+  useMaterial3: false,
+  appBarTheme: const AppBarTheme(),
+);
